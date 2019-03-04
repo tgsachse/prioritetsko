b9b41ad7a57b7a3c472e9b3869211ba6d4cc4531
Tiger Sachse <tiger@sachse.io>
2019-03-03 19:47:18 -0500

    comment drive

--- Source/Prioritetsko/SequentialPriorityQueue.java
+++ Source/Prioritetsko/SequentialPriorityQueue.java
@@ -1,14 +1,17 @@
+// A sequential (single-threaded) priority queue from scratch.
+// Written by Tiger Sachse.
+
 package Prioritetsko;
 
 import java.util.*;
 
+// Provides a single-threaded, generic priority queue.
 public class SequentialPriorityQueue<E extends Comparable<E>> {
     private ArrayList<E> elements;
 
     // Create a new, empty priority queue.
     public SequentialPriorityQueue() {
         elements = new ArrayList<E>();
-        elements.add(null);
     }
 
     /*
@@ -19,23 +22,35 @@ public class SequentialPriorityQueue<E extends Comparable<E>> {
     //public SequentialPriorityQueue(Elements to enqueue)
 
 
+    // Add an element to the priority queue.
     public void insert(E element) {
+
+        // The element is added to the end of the elements list and then is
+        // percolated up to its appropriate position.
         elements.add(element);
-        percolateElementUp(elements.size() - 1);
+        percolateElementUp(getMaxIndex());
     }
 
+    // Get and remove the element at the front of the priority queue.
     public E retrieve() throws EmptyQueueException {
+
+        // Attempt to retrieve the front element. If the queue is empty, throw
+        // an exception.
         E retrievedElement;
         try {
-            retrievedElement = elements.get(1);
+            retrievedElement = elements.get(0);
         }
         catch (IndexOutOfBoundsException exception) {
             throw new EmptyQueueException("The priority queue is empty!");
         }
 
+        // Attempt to move the element at the back of the elements list to the
+        // front, then percolate that element down. If this causes an index
+        // error (because the retrieved element from earlier was the only
+        // element in the queue) then do nothing.
         try {
-            elements.set(1, elements.remove(elements.size() - 1));
-            percolateElementDown(1);
+            elements.set(0, elements.remove(getMaxIndex()));
+            percolateElementDown(0);
         }
         catch (IndexOutOfBoundsException exception) {
         }
@@ -43,50 +58,65 @@ public class SequentialPriorityQueue<E extends Comparable<E>> {
         return retrievedElement;
     }
 
+    // Get the element at the front of the priority queue.
     public E peek() throws EmptyQueueException {
         try {
-            return elements.get(1);
+            return elements.get(0);
         }
         catch (IndexOutOfBoundsException exception) {
             throw new EmptyQueueException("The priority queue is empty!");
         }
     }
 
-    // make iterable
 
+    // Get a string representation of this priority queue.
+    @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
 
-        for (int elementIndex = 1; elementIndex < (elements.size() - 1); elementIndex++) {
+        for (int elementIndex = 1; elementIndex < (getMaxIndex()); elementIndex++) {
             stringBuilder.append(elements.get(elementIndex));
             stringBuilder.append(" ");
         }
-        stringBuilder.append(elements.get(elements.size() - 1));
+        stringBuilder.append(elements.get(getMaxIndex()));
 
         return stringBuilder.toString();
     }
 
+    // Check if the priority queue is empty.
     public boolean isEmpty() {
-        return elements.size() < 2;
+        return elements.isEmpty();
     }
 
+    // Get a parent's left child's index.
     private int getLeftChildIndex(int parentIndex) {
-        return parentIndex * 2;
-    }
-
-    private int getRightChildIndex(int parentIndex) {
         return (parentIndex * 2) + 1;
     }
 
-    private int getParentIndex(int childIndex) {
-        return childIndex / 2;
+    // Get a parent's right child's index.
+    private int getRightChildIndex(int parentIndex) {
+        return (parentIndex * 2) + 2;
     }
 
+    // Get a child's parent's index.
+    private int getParentIndex(int childIndex) {
+        return (childIndex - 1) / 2;
+    }
+
+    // Get the maximum index currently occupied in the priority queue.
+    private int getMaxIndex() {
+        return elements.size() - 1;
+    }
+
+    // Move an element up through the priority queue to its proper place.
     private void percolateElementUp(int initialIndex) {
         int currentIndex = initialIndex;
         int parentIndex = getParentIndex(currentIndex);
 
-        while (parentIndex >= 1) {
+        // While the parent index of the current element is in bounds, if the
+        // parent element is larger than the current element: swap the two and
+        // repeat.
+        while (parentIndex >= 0) {
             E parentElement = elements.get(parentIndex);
             E currentElement = elements.get(currentIndex);
 
@@ -101,12 +131,17 @@ public class SequentialPriorityQueue<E extends Comparable<E>> {
         }
     }
 
+    // Move an element down through the priority queue to its proper place.
     private void percolateElementDown(int initialIndex) {
         int currentIndex = initialIndex;
         int leftChildIndex = getLeftChildIndex(currentIndex);
         int rightChildIndex = getRightChildIndex(currentIndex);
 
-        while (leftChildIndex <= (elements.size() - 1)) {
+        while (leftChildIndex <= (getMaxIndex())) {
+
+            // Load the left child and current element. Attempt to load the
+            // right child as well. If it does not exist, swap the current
+            // element with the left child (if the left child is larger).
             E currentElement = elements.get(currentIndex);
             E leftChildElement = elements.get(leftChildIndex);
             E rightChildElement;
@@ -120,6 +155,8 @@ public class SequentialPriorityQueue<E extends Comparable<E>> {
                 break;
             }
 
+            // Swap with the larger of the two children if the child is larger
+            // than the current element.
             if (leftChildElement.compareTo(rightChildElement) < 0) {
                 if (leftChildElement.compareTo(currentElement) < 0) {
                     Collections.swap(elements, currentIndex, leftChildIndex);
