8a633c1d0b6eb1a0485a77f3c5d5493d120919cc
Chris <guitartaliaferro@yahoo.com>
2019-04-09 18:30:29 -0400

    Added skeleton for ECPriorityQueue

---
+++ Source/Prioritetsko/ECPriorityQueue.java
@@ -0,0 +1,21 @@
+// Adaptive Priority Queue with Elimination and Combining.
+// Written by Christopher Taliaferro & Ben Faria
+
+package Prioritetsko;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+public class ECPriorityQueue<E extends Comparable<E>> implements PriorityQueue<E> {
+    public ECPriorityQueue () {
+
+    }
+
+    public void insert(E element) {
+
+    }
+
+    public E retrieve() throws EmptyQueueException {
+        
+    }
+}
